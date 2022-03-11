2e89e0c4a2c26fddcb1dba1b82a20e86b40eb8e6
Mino Cha (Isaac) <mino.cha@emory.edu>
2022-03-11 14:59:02 -0500

    connect BE

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,42 @@
-import React from "react";
+// BE on Server
+// http://emazon-backend.herokuapp.com/api/products
+// BE on local
+// http://localhost:8888/api/products
+
+import React, { useState, useEffect } from "react";
 import "./Home.css";
 import Product from "./Product";
+import axios from 'axios';
 
 function Home() {
+  const [list, setList] = useState(true);
+  const [card, setCard] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    fetch("http://emazon-backend.herokuapp.com/api/products")
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setProducts(responseJson.data);
+      });
+  }, []);
+  
+  let showCard = (id) => {
+    fetch(`http://emazon-backend.herokuapp.com/api/products/${id}`)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setProduct(responseJson.data);
+        setList(false);
+        setCard(true);
+      });
+  };
+  
+  let showList = () => {
+    setCard(false);
+    setList(true);
+  };
+
   return (
     <div className="home">
       <div className="home__container">
@@ -11,6 +45,30 @@ function Home() {
           src="https://github.com/yikevding/emazon/blob/main/emory.jpeg?raw=true"
           alt=""
         />
+        <div className="container">
+          {list ? (
+            <div className="home__row">
+              {products.map((product, i) => (
+                <Product
+                  title={product.name}
+                  price={product.price}
+                  rating={product.rating}
+                  image={product.image}
+                />
+              ))}
+            </div>
+          ) : null}
+          {card ? (
+            <div class="home__row">
+              <Product
+                title={product.name}
+                price={product.price}
+                rating={product.rating}
+                image={product.image}
+              />
+            </div>
+          ) : null}
+        </div>
 
         <div className="home__row">
           <Product
@@ -27,7 +85,7 @@ function Home() {
           />
         </div>
 
-        <div className="home__row">
+        {/* <div className="home__row">
           <Product
             title="White Marble Dinning Table"
             price={399.49}
@@ -56,7 +114,7 @@ function Home() {
             rating={4}
             image="https://github.com/yikevding/emazon/blob/main/image/monitor.jpeg?raw=true"
           />
-        </div>
+        </div> */}
       </div>
     </div>
   );
